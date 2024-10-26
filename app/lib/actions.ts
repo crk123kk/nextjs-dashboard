963c2c6e9ed5833d826e94b63fedf55605a27adf
陈墨 <1973657048@qq.com>
2024-10-26 12:13:14 +0800

    feat: 发票的增删改查

---
+++ app/lib/actions.ts
@@ -0,0 +1,63 @@
+"use server";
+import { sql } from "@vercel/postgres";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  // 金额字段专门设置为从字符串强制（更改）为数字，同时还验证其类型。
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  // 通常最好的做法是在数据库中存储以美分为单位的货币值，以消除 JavaScript 浮点错误并确保更高的准确性。
+  const amountInCents = amount * 100;
+  // 我们为发票的创建日期创建一个格式为“YYYY-MM-DD”的新日期：
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // 只有加了这个，进到新页面，才会重新渲染并且更新数据，这就是因为 nextJs 缓存的原因。
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
